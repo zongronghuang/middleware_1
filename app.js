ab00617fc0180d3211219a42a79de824b20d2e26
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-05-01 00:34:30 +0800

    Create GUI for triggering routes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const port = 3000
 
-// 顯示所有
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(methodOverride('_method'))
+
+
+// 顯示所有路由 request 的資訊
 app.all('*', (req, res, next) => {
   const now = new Date().toISOString()
   const time = now.substring(0, 10) + ' ' + now.substring(11, 19)
   const method = req.method
-  const path = req.originalUrl
+  const path = req.baseUrl + req.path
 
   // 避免顯示 favicon request
   if (path !== '/favicon.ico') {
@@ -18,26 +26,26 @@ app.all('*', (req, res, next) => {
 
 // 列出全部 Todo
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  res.render('index', { message: '列出全部 todo' })
 })
 
 // 新增一筆 Todo 頁面
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  res.render('index', { message: '新增 Todo 頁面' })
 })
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  res.render('index', { message: '顯示一筆 Todo' })
 })
 
 // 新增一筆  Todo
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+  res.render('index', { message: '新增一筆  Todo' })
 })
 
 app.delete('/:id/delete', (req, res) => {
-  res.send('刪除 Todo')
+  res.render('index', { message: '刪除 Todo' })
 })
 
 app.listen(port, () => {
